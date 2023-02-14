57f3cc392e81d17117da94b5c53b954cd7cb6a78
alexdracma <alexdracma@gmail.com>
2023-02-14 10:12:12 +0100

    Añadido idioma español

---
+++ lang/es/passwords.php
@@ -0,0 +1,22 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Password Reset Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are the default lines which match reasons
+    | that are given by the password broker for a password update attempt
+    | has failed, such as for an invalid token or invalid new password.
+    |
+    */
+
+    'reset' => '¡Tu contraseña ha sido restablecida!',
+    'sent' => '¡Te hemos mandado un correo con el enlace para restablecer tu contraseña!',
+    'throttled' => 'Por favor espere antes de intentarlo de nuevo.',
+    'token' => 'El token de cambio de contraseña es incorrecto.',
+    'user' => "No podemos encontrar a ningún usuario con ese correo.",
+
+];
